aedaf527b43e6b9c06a33b9ad395095ff7196cef
Casey Waweru <casey.waweru@gmail.com>
2026-06-17 13:44:50 +0300

    fix: correcting main.py and adding a generate_log() function

---
+++ main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+'''
+'''
+
+from datetime import datetime
+import requests
+
+def generate_log():
+    log_data = ["User logged in", "User updated profile", "Report exported"]
+    filename = f"log_{datetime.now().strftime('%Y%m%d')}.txt"
+
+    with open(filename, "w") as file:
+        for entry in log_data:
+            file.write(f"{entry}\n")
+
+    return (f"Log written to {filename}")
+
+def fetch_data():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+    if response.status_code == 200:
+        return response.json()
+
+if __name__ == "__main__":
+    post = fetch_data()
+    print("Fetched Post Title:", post.get("title", "No title found"))
+    log = generate_log()
+    print(log)
